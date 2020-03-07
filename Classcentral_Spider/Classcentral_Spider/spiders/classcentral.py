05b1591a03cc9456195dead4da8a5fe37e492a37
syam <alustea@gmail.com>
2020-03-07 22:55:51 +0700

    Crawl subject URLs

---
+++ Classcentral_Spider/Classcentral_Spider/spiders/classcentral.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy import Spider
+from scrapy.http import Request
+
+
+class ClasscentralSpider(Spider):
+    name = 'classcentral'
+    allowed_domains = ['classcentral.com']
+    start_urls = ['https://www.classcentral.com/subjects']
+
+    def __init__(self, subject=None):
+        self.subject = subject
+
+    def parse(self, response):
+        if self.subject:
+            subject_url = response.xpath('//a[contains(@title, "Programming")]/@href').extract_first()
+            absolute_subject_url = response.urljoin(subject_url)
+            yield Request(absolute_subject_url,
+                          callback=self.parse_subject)
+        else:
+            subjects = response.xpath('//h3/a[1]/@href').extract()
+            for subject in subjects:
+                absolute_subject_url = response.urljoin(subject)
+                yield Request(absolute_subject_url,
+                              callback=self.parse_subject)
+
+    def parse_subject(self, response):
+        pass
